a303c68beee929b284fa2b9d0dbd432e88aed2c2
NaumanSaleem <noumansaleem1999@gmail.com>
2024-02-22 16:48:00 +0500

    Initial commit + header added

---
+++ controllers/controllers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import base64
+
+from odoo.http import request
+from odoo import http
+
+
+class MdWebsite(http.Controller):
+    @http.route('/', auth='public', type='http', website=True)
+    def index(self, **kw):
+        return request.render('7md_website.home')
