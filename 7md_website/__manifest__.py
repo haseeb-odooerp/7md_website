b4de8990a8f9ae62cc5da4652191780e528e0e58
umair <umair@vistatrans.com>
2024-03-18 14:16:46 +0500

    [UPD] Custom Modules

---
+++ 7md_website/__manifest__.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "7md_website",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        this module is for 7md ecommerce store
+    """,
+
+    'author': "OAKLAND - odooERP.ae",
+    'website': "http://www.odooerp.ae",
+
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','website', 'website_sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates/header.xml',
+        'templates/index.xml',
+        'templates/about.xml',
+        'templates/contact.xml',
+        'templates/product_test_template.xml',
+        'templates/credential_layout.xml',
+        'templates/footer.xml',
+        'templates/header_searchbar_custom.xml',
+        'templates/product_details_screen.xml'
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'assets': {
+        'web.assets_frontend':[
+            "7md_website/static/src/js/landing.js",
+            "7md_website/static/src/css/account.css",
+            "7md_website/static/src/css/all.css",
+            "7md_website/static/src/css/slick-theme.css",
+            "7md_website/static/src/css/slick.css",
+            "7md_website/static/src/css/bootstrap-grid.css",
+            "7md_website/static/src/css/bootstrap-grid.rtl.css",
+            "7md_website/static/src/css/bootstrap-reboot.css",
+            "7md_website/static/src/css/bootstrap-reboot.rtl.css",
+            "7md_website/static/src/css/bootstrap-utilities.css",
+            "7md_website/static/src/css/bootstrap-utilities.rtl.css",
+            "7md_website/static/src/css/bootstrap.css",
+            "7md_website/static/src/css/bootstrap.rtl.css",
+            "7md_website/static/src/css/layout-767.css",
+            "7md_website/static/src/css/layout-992.css",
+            # "7md_website/static/src/css/layout-ar.css",
+            "7md_website/static/src/css/layout.css",
+            "7md_website/static/src/css/main-page.css",
+            "7md_website/static/src/css/product-details.css",
+            "7md_website/static/src/css/shopping-cart.css",
+            "7md_website/static/src/css/updated-styles.scss",
+            # "7md_website/static/src/js/bootstrap.bundle.min.js",
+            # "7md_website/static/src/js/bootstrap.bundle.min.js.map",
+            
+            
+        ],
+    },
+}
---
+++ 7md_website/controllers/controllers.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import base64
+
+from odoo.http import request
+from odoo import http
+
+
+class MdWebsite(http.Controller):
+    @http.route('/', auth='public', type='http', website=True)
+    def index(self, **kw):
+        return request.render('7md_website.home')
+    
+
+class about7md(http.Controller):
+    @http.route('/about', auth='public', type='http', website=True)
+    def index(self, **kw):
+        return request.render('7md_website.about')
+    
+
+class contact7md(http.Controller):
+    @http.route('/contact', auth='public', type='http', website=True)
+    def index(self, **kw):
+        return request.render('7md_website.contact')
+    
+
+class websitePolicies(http.Controller):
+
+    @http.route('/privacy_policy', auth='public', type='http', website=True)
+    def privacyPolicy7md(self, **kw):
+
+        return request.render('privacy_policy_7md')
+
+    @http.route('/return_policy', auth='public', type='http', website=True)
+    def returnPolicy(self, **kw):
+
+        return request.render('return_policy_7md')
+
+
+    @http.route('/terms_and_conditions', auth='public', type='http', website=True)
+    def termAndCondition(self, **kw):
+        
+        return request.render('terms_and_condition_7md')
+    
